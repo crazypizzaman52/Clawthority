ea9022ecc265af3e79f4c451a7195576a8b084f7
crazypizzaman52 <thekaicoleman@gmail.com>
2025-02-01 10:50:57 -0500

    updated to arcade drive and added sparkmax config

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,45 +1,110 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.PS4Controller;
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.PS4Controller;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends TimedRobot {
-  private DifferentialDrive m_myRobot;
-  private PS4Controller joystick;
-  private static final int leftDeviceID = 1; 
-  private static final int rightDeviceID = 2;
-  private SparkMax m_leftMotor;
-  private SparkMax m_rightMotor;
+  SparkMax leftLeader;
+  SparkMax rightLeader;
+  PS4Controller joystick;
 
-  @Override
-  public void robotInit() {
-  /**
-   * SPARK MAX controllers are intialized over CAN by constructing a CANSparkMax object
-   * 
-   * The CAN ID, which can be configured using the SPARK MAX Client, is passed as the
-   * first parameter
-   * 
-   * The motor type is passed as the second parameter. Motor type can either be:
-   *  com.revrobotics.CANSparkLowLevel.MotorType.kBrushless
-   *  com.revrobotics.CANSparkLowLevel.MotorType.kBrushed
-   * 
-   * The example below initializes four brushless motors with CAN IDs 1 and 2. Change
-   * these parameters to match your setup
-   */
-    m_leftMotor = new SparkMax(leftDeviceID, MotorType.kBrushless);
-    m_rightMotor = new SparkMax(rightDeviceID, MotorType.kBrushless);
+  public Robot() {
+    // Initialize the SPARKs
+    leftLeader = new SparkMax(1, MotorType.kBrushless);
+    rightLeader = new SparkMax(3, MotorType.kBrushless);
 
-    m_myRobot = new DifferentialDrive(m_leftMotor, m_rightMotor);
+    /*
+     * Create new SPARK MAX configuration objects. These will store the
+     * configuration parameters for the SPARK MAXes that we will set below.
+     */
+    SparkMaxConfig globalConfig = new SparkMaxConfig();
+    SparkMaxConfig rightLeaderConfig = new SparkMaxConfig();
 
+    /*
+     * Set parameters that will apply to all SPARKs. We will also use this as
+     * the left leader config.
+     */
+    globalConfig
+        .smartCurrentLimit(50)
+        .idleMode(IdleMode.kBrake)
+        .openLoopRampRate(0.25);
+
+    // Apply the global config and invert since it is on the opposite side
+    rightLeaderConfig
+        .apply(globalConfig)
+        .inverted(true);
+
+    leftLeader.configure(globalConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    rightLeader.configure(rightLeaderConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    // Initialize joystick
     joystick = new PS4Controller(0);
   }
 
   @Override
+  public void robotPeriodic() {
+    // Display the applied output of the left and right side onto the dashboard
+    SmartDashboard.putNumber("Left Out", leftLeader.getAppliedOutput());
+    SmartDashboard.putNumber("Right Out", rightLeader.getAppliedOutput());
+  }
+
+  @Override
+  public void autonomousInit() {
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+  }
+
+  @Override
+  public void teleopInit() {
+  }
+
+  @Override
   public void teleopPeriodic() {
-    m_myRobot.tankDrive(joystick.getLeftY(), joystick.getRightY());
+    /**
+     * Get forward and rotation values from the joystick. Invert the joystick's
+     * Y value because its forward direction is negative.
+     */
+    double forward = -joystick.getLeftY();
+    double rotation = joystick.getRightX();
+
+    /*
+     * Apply values to left and right side. We will only need to set the leaders
+     * since the other motors are in follower mode.
+     */
+    leftLeader.set(forward + rotation);
+    rightLeader.set(forward - rotation);
+  }
+
+  @Override
+  public void disabledInit() {
+  }
+
+  @Override
+  public void disabledPeriodic() {
+  }
+
+  @Override
+  public void testInit() {
+  }
+
+  @Override
+  public void testPeriodic() {
+  }
+
+  @Override
+  public void simulationInit() {
+  }
+
+  @Override
+  public void simulationPeriodic() {
   }
 }
