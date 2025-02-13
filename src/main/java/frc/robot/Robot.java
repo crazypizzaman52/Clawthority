ec7cdeb786a2921f018afe65da056497c27d8f15
crazypizzaman52 <thekaicoleman@gmail.com>
2025-02-12 21:51:43 -0500

    added follower motors for new drivetrain

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,25 +1,29 @@
 package frc.robot;
 
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.config.SparkMaxConfig;
-import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
-import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
 
-import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.PS4Controller;
+import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends TimedRobot {
   SparkMax leftLeader;
   SparkMax rightLeader;
+  SparkMax leftFollower;
+  SparkMax rightFollower;
   PS4Controller joystick;
 
   public Robot() {
     // Initialize the SPARKs
     leftLeader = new SparkMax(1, MotorType.kBrushless);
     rightLeader = new SparkMax(3, MotorType.kBrushless);
+    leftFollower = new SparkMax(2, MotorType.kBrushless);
+    rightFollower = new SparkMax(4, MotorType.kBrushless);
 
     /*
      * Create new SPARK MAX configuration objects. These will store the
@@ -27,6 +31,8 @@ public class Robot extends TimedRobot {
      */
     SparkMaxConfig globalConfig = new SparkMaxConfig();
     SparkMaxConfig rightLeaderConfig = new SparkMaxConfig();
+    SparkMaxConfig leftFollowerConfig = new SparkMaxConfig();
+    SparkMaxConfig rightFollowerConfig = new SparkMaxConfig();
 
     /*
      * Set parameters that will apply to all SPARKs. We will also use this as
@@ -35,15 +41,28 @@ public class Robot extends TimedRobot {
     globalConfig
         .smartCurrentLimit(50)
         .idleMode(IdleMode.kBrake)
-        .openLoopRampRate(0.25);
+        .openLoopRampRate(0.1);
 
     // Apply the global config and invert since it is on the opposite side
     rightLeaderConfig
         .apply(globalConfig)
         .inverted(true);
 
+    // Apply the global config and set the leader SPARK for follower mode
+    leftFollowerConfig
+        .apply(globalConfig)
+        .follow(leftLeader);
+
+    // Apply the global config and set the leader SPARK for follower mode
+    rightFollowerConfig
+        .apply(globalConfig)
+        .follow(rightLeader);
+
     leftLeader.configure(globalConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    leftFollower.configure(leftFollowerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
     rightLeader.configure(rightLeaderConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    rightFollower.configure(rightFollowerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
     // Initialize joystick
     joystick = new PS4Controller(0);
   }
